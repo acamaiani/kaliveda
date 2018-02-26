7b247001ac748e23cab6ba669d7cda7a8a67cdf5
John Frankland <frankland@ganil.fr>
2018-02-26 14:22:59 +0100

    do not reconstruct gammas in INDRA data
    if a CsI fires, we try the identification straight away.
    if a gamma is identified, we do not start a particle reconstruction.
    the number of gammas in the event is in event parameter INDRA_GAMMA_MULT

--- KVMultiDet/events/KVEvent.cpp
+++ KVMultiDet/events/KVEvent.cpp
@@ -963,6 +963,7 @@ const Char_t* KVEvent::GetPartitionName()
 void KVEvent::MergeEventFragments(TCollection* events, Option_t* opt)
 {
    // Merge all events in the list into one event (this one)
+   // We also merge/sum the parameter lists of the events
    // First we clear this event, then we fill it with copies of each particle in each event
    // in the list.
    // If option "opt" is given, it is given as argument to each call to
@@ -979,6 +980,7 @@ void KVEvent::MergeEventFragments(TCollection* events, Option_t* opt)
       while ((n = e->GetNextParticle())) {
          n->Copy(*AddParticle());
       }
+      GetParameters()->Merge(*(e->GetParameters()));
       e->Clear(opt);
    }
 }
